2bdc92911e36c8b503c226dfa78986045d518684
Baxfain <severinusfabian@gmail.com>
2025-11-20 14:24:26 +0700

    balek

--- src/main/java/com/example/controllers/LoginServlet.java
+++ src/main/java/com/example/controllers/LoginServlet.java
@@ -2,9 +2,13 @@ package com.example.controllers;
 
 import com.example.dao.UserDAO;
 import com.example.models.User;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.*;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.io.IOException;
 import java.io.IOException;
 
 @WebServlet("/LoginServlet")
