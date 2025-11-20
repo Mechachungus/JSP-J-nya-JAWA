1f42505874a8c704c1d06af3782561c5c4e97859
Baxfain <severinusfabian@gmail.com>
2025-11-20 16:00:26 +0700

    pom.xml mysql

--- src/main/java/com/example/controllers/LoginServlet.java
+++ src/main/java/com/example/controllers/LoginServlet.java
@@ -9,7 +9,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import java.io.IOException;
-import java.io.IOException;
 
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
