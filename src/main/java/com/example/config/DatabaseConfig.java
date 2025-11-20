8b77d5a3ab24ab2776c4e83dc21779e922514d5f
Baxfain <severinusfabian@gmail.com>
2025-11-20 18:13:21 +0700

    pake postgres

--- src/main/java/com/example/config/DatabaseConfig.java
+++ src/main/java/com/example/config/DatabaseConfig.java
@@ -2,15 +2,16 @@ package com.example.config;
 
 public class DatabaseConfig {
     // Database credentials dari freesqldatabase.com
-    private static final String DB_HOST = "sql109.infinityfree.com";
-    private static final String DB_PORT = "3306";
-    private static final String DB_NAME = "if0_40464518_eastout";
-    private static final String DB_USER = "f0_40464518";
-    private static final String DB_PASSWORD = "aJ3LsDvy4h";
+    private static final String DB_HOST = "db.joepctttmwjxojncyepf.supabase.co";
+    private static final String DB_PORT = "5432";
+    private static final String DB_NAME = "postgres";
+    private static final String DB_USER = "postgres";
+    private static final String DB_PASSWORD = "STEVENFEKFRENBGT";
+
+    private static final String DRIVER_CLASS_NAME = "org.postgresql.Driver";
     
     // JDBC URL
-    private static final String DB_URL = "jdbc:mysql://" + DB_HOST + ":" + DB_PORT + "/" + DB_NAME 
-            + "?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+    private static final String DB_URL = "jdbc:postgresql://" + DB_HOST + ":" + DB_PORT + "/" + DB_NAME;
 
     // Getter methods
     public static String getDbUrl() {
@@ -27,6 +28,6 @@ public class DatabaseConfig {
     
     // Driver class name
     public static String getDriverClassName() {
-        return "com.mysql.cj.jdbc.Driver";
+        return DRIVER_CLASS_NAME;
     }
 }
