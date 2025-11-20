5f0a1b699de9e651be79016a91f4cdb7b187abb9
Baxfain <severinusfabian@gmail.com>
2025-11-20 21:30:45 +0700

    AAAKKHHH

--- src/main/java/com/example/config/DatabaseConfig.java
+++ src/main/java/com/example/config/DatabaseConfig.java
@@ -12,9 +12,9 @@ public class DatabaseConfig {
     
     // JDBC URL
     private static final String DB_URL = "jdbc:postgresql://" + DB_HOST + ":" + DB_PORT + "/" + DB_NAME 
-        + "?sslmode=require"
-        + "&ssl=true"
-        + "&sslfactory=org.postgresql.ssl.NonValidatingFactory";
+            + "?sslmode=verify-full"
+            + "&ssl=true"
+            + "&sslfactory=org.postgresql.ssl.DefaultSSLFactory";
 
     // Getter methods
     public static String getDbUrl() {
